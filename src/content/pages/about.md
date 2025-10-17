7acb7d1d216e63f4280951c0bb43ceaa3c7acd6b
aejtn <tejanarayana05@gmail.com>
2025-10-17 16:04:57 +0530

    Updated

--- src/content/pages/about.md
+++ src/content/pages/about.md
@@ -10,11 +10,9 @@ seo:
 
 ![Alt text for image](/about.jpeg)
 
-**Note!:** This about page is entirely fictional and created for the purpose of showcasing [Dante Astro.js theme functionality](https://justgoodui.com/astro-themes/dante/).
+## Teja Narayana
 
-## Ethan Donovan - Web Developer based in Estonia
-
-**Greetings!** I'm Ethan Donovan, a passionate web developer residing in the picturesque landscapes of [Estonia](https://en.wikipedia.org/wiki/Estonia). With a keen eye for design and a love for crafting seamless digital experiences, I bring a unique blend of creativity and technical expertise to the world of web development.
+**Namaste!** I'm Teja. I'm an engineer passionate about product management/ownership, user experience, AI/ML and Data Science. I thrive in making requirements into ergonomic/feasible/innovative/user-centric outcomes.
 
 ## Skills and Expertise
 
--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -39,18 +39,36 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    website: 'https://example.com',
-    title: 'Dante',
-    subtitle: 'Minimal Astro.js theme',
-    description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
+    website: 'https://www.aejt.in',
+    title: 'Teja',
+    subtitle: 'Space of Teja Narayana',
+    description: 'Welcome to the world of Teja. Let\'s get to know each other',
     image: {
-        src: '/dante-preview.jpg',
-        alt: 'Dante - Astro.js and Tailwind CSS theme'
+        src: '/teja-preview.jpg',
+        alt: 'Space of Teja Narayana'
     },
     headerNavLinks: [
         {
             text: 'Home',
             href: '/'
+        },
+        {
+            text: 'About',
+            href: '/about'
+        },
+        {
+            text: 'Projects',
+            href: '/projects'
+        },
+        {
+            text: 'Blog',
+            href: '/blog'
+        }
+    ],
+    footerNavLinks: [
+        {
+            text: 'Contact',
+            href: '/contact'
         },
         {
             text: 'Projects',
@@ -65,56 +83,27 @@ const siteConfig: SiteConfig = {
             href: '/tags'
         }
     ],
-    footerNavLinks: [
-        {
-            text: 'About',
-            href: '/about'
-        },
-        {
-            text: 'Contact',
-            href: '/contact'
-        },
-        {
-            text: 'Terms',
-            href: '/terms'
-        },
-        {
-            text: 'Download theme',
-            href: 'https://github.com/JustGoodUI/dante-astro-theme'
-        }
-    ],
     socialLinks: [
         {
-            text: 'Dribbble',
-            href: 'https://dribbble.com/'
+            text: 'LinkedIn',
+            href: 'https://www.linkedin.com/in/narayana-teja/'
         },
         {
             text: 'Instagram',
-            href: 'https://instagram.com/'
+            href: 'https://instagram.com/aejtn'
         },
         {
-            text: 'X/Twitter',
-            href: 'https://twitter.com/'
+            text: 'Email',
+            href: 'mailto:tj@aejt.in'
         }
     ],
     hero: {
-        title: 'Hi There & Welcome to My Corner of the Web!',
-        text: "I'm **Ethan Donovan**, a web developer at Amazing Studio, dedicated to the realms of collaboration and artificial intelligence. My approach involves embracing intuition, conducting just enough research, and leveraging aesthetics as a catalyst for exceptional products. I have a profound appreciation for top-notch software, visual design, and the principles of product-led growth. Feel free to explore some of my coding endeavors on <a href='https://github.com/JustGoodUI/dante-astro-theme'>GitHub</a> or follow me on <a href='https://twitter.com/justgoodui'>Twitter/X</a>.",
+        title: 'Hello, Welcome to my space 🙏',
+        text: "I'm **Teja Narayana**, currently a Product Analyst Intern at <a href='https://www.gxco.org'>GXCO</a> , dedicated to the realms of product management, data analytis and artificial intelligence. I believe the best products are built with total empathy. My work as a Product Analyst Intern is a continuous exercise in shifting perspective: I dive into the technical code to find innovative solutions, but I first place myself squarely in the end-user's mind to understand the true need. This human-factored, ergonomic, and user-centric philosophy ensures that the solutions I design—from OOD to final feature—are not just technically correct, but genuinely provide the best experience for the client, the developer, and the business.",
         image: {
             src: '/hero.jpeg',
-            alt: 'A person sitting at a desk in front of a computer'
-        },
-        actions: [
-            {
-                text: 'Get in Touch',
-                href: '/contact'
-            }
-        ]
-    },
-    subscribe: {
-        title: 'Subscribe to Dante Newsletter',
-        text: 'One update per week. All the latest posts directly in your inbox.',
-        formUrl: '#'
+            alt: 'Picture of Teja'
+        }
     },
     postsPerPage: 8,
     projectsPerPage: 8
